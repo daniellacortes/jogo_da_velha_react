4548de5aec8e3401d0a5bb48874e85aaeb887e61
Daniella Brito Côrtes <danibcortes@hotmail.com>
2024-08-30 16:13:07 -0300

    Do jeito que tá (cheio de erros)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square ({ value, onSquareClick }){
+function Square ({ value, onSquareClick}){
 
   return (
     <button 
@@ -13,7 +13,21 @@ function Square ({ value, onSquareClick }){
 function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);  
   let status;
+  let divArray = new Array(3).fill(null);
+  let squaresArray = new Array(3).fill(null);
 
+  let squaresTry = squares.map((item, id) => (
+    <Square key={id}  value={item} onSquareClick={() => handleClick(id)}/>
+));
+
+
+  let divMap = divArray.map((item, id) => (
+    <div key={id} className="board-row"> 
+    {squaresArray.map((item, id2) => (
+    <Square key={id*3+id2}  value={squares[id*3+id2]} onSquareClick={() => handleClick(id*3+id2)}/>
+  ))}
+    </div>));
+  
   if (winner) {
     status = "Winner: " + winner;
   } else {
@@ -21,6 +35,7 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   function handleClick(i) {
+    setCurrentMoveOrder(moveOrder);
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
@@ -40,6 +55,10 @@ function Board({ xIsNext, squares, onPlay }) {
     <>
     <div className="status">{status}</div>
     <div className="board-row">
+    {divMap}
+    </div>
+
+    {/* <div className="board-row">
     <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
     <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
     <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>      
@@ -53,7 +72,7 @@ function Board({ xIsNext, squares, onPlay }) {
     <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
     <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>       
-    </div>
+    </div> */}
     </>
   )
   
@@ -79,11 +98,16 @@ function calculateWinner(squares) {
   return null;
 }
 
+
+
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;  
   const currentSquares = history[currentMove];
+  let descriptionCurrentMove = 'Current move: #' + (currentMove+1);
+  
+ 
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -97,6 +121,7 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
@@ -109,13 +134,40 @@ export default function Game() {
     );
   });
 
+  const [currentMoveOrder, setCurrentMoveOrder] = useState(moves);
+
+    let moveOrder = moves.map((item, id) => 
+      <li key={id}>{item.props.children}</li>)
+    setCurrentMoveOrder({moveOrder});
+
+  // setCurrentMoveOrder(moveOrder);
+
+  function Order (){
+    // let moveOrderOrder = currentMoveOrder.reverse().map((item, id) => 
+    //   <li key={id}>{item}</li> );
+    // setCurrentMoveOrder(moveOrderOrder);
+
+
+    console.log ("Tá revertendo?", moveOrderOrder);
+
+    // moveOrder = moveOrderOrder;
+    // return moveOrder;
+
+   };
+
+  console.log (moves);
+
   return (
     <div className="game">
       <div className="game-board">
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-      <ol>{moves}</ol>
+      <ol>{currentMoveOrder}</ol>
+      <button onClick={Order}>Ordenar</button>
+      
+      <p>{descriptionCurrentMove}
+      </p>
       </div>
     </div>
   );
